36379b0850733da53e1887d43e7cecdaa91d8c9f
Nicu-Evelin Pîntea <nicu_evelin.pintea@stud.acs.upb.ro>
2026-09-11 20:26:36 +0300

    Apply SMOTE oversampling

--- train.py
+++ train.py
@@ -1,13 +1,21 @@
 # train a Random Forest
 import numpy as np
 
+from imblearn.over_sampling import SMOTE
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 from prepare import load_and_prepare
 
 X_train, X_test, y_train, y_test = load_and_prepare()
 print(f"\nTraining on {len(X_train)} flows")
-model = RandomForestClassifier(n_estimators=100, random_state=42, n_jobs=-1, class_weight="balanced")
+model = RandomForestClassifier(n_estimators=100, random_state=42, n_jobs=-1)
+# model = RandomForestClassifier(n_estimators=100, random_state=42, n_jobs=-1, class_weight="balanced")
+print("\nBefore SMOTE:")
+print(y_train.value_counts())
+smote = SMOTE(random_state=42, k_neighbors=3)
+X_train, y_train = smote.fit_resample(X_train, y_train)
+print("\nAfter SMOTE:")
+print(y_train.value_counts())
 model.fit(X_train, y_train)
 print("Predicting on the test set...")
 y_pred = model.predict(X_test)
